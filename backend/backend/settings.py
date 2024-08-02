6db0884c55ba4329fe4b1cc8920a90b91415404d
abenjamin5-collab <abenjamin5@live.maryville.edu>
2024-08-02 14:28:52 -0400

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tennis_booking',
         'USER': 'root',
-        'PASSWORD': 'Maryville',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
